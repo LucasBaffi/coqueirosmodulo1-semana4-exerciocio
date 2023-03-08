90d7d9071d625812979f0079582f1338148fd49f
Lucas <lucasrenatobaffi@hotmail.com>
2023-03-07 23:26:26 -0300

    criacao classe menu

--- ExerciciosSemana4/Program.cs
+++ ExerciciosSemana4/Program.cs
@@ -1,71 +1,14 @@
-﻿
-using drink;
+﻿using drink;
 using juice;
 using refrigerant;
+ 
+ 
+  List<Bebida> bebidas = new List<Bebida>();
 
-namespace ExercicioSemana4
-{
-    public class Repositorio
-    {
-        private static List<Bebida> ListaBebida = new List<Bebida>();
-        private static List<Suco> ListaSuco = new List<Suco>();
-        private static List<Refrigerante> ListaRefrigerante = new List<Refrigerante>();
+  
+public class Menu{
 
-        public static void AdicionarSuco(Suco suco)
-        {
-            ListaBebida.Add(suco);
-            ListaSuco.Add(suco);
-        }
+public void inserirBebidas(){
 
-        public static void AdicionarRefrigerante(Refrigerante refrigerante)
-        {
-            ListaBebida.Add(refrigerante);
-            ListaRefrigerante.Add(refrigerante);
-        }
-
-        public static void AdicionarBebida(Bebida bebida)
-        {
-            ListaBebida.Add(bebida);
-        }
-
-        public static void AlterarBebida(Bebida bebida)
-        {
-            var index = ListaBebida.FindIndex(b => b.Id == bebida.Id);
-            if (index != -1)
-            {
-                ListaBebida[index] = bebida;
-            }
-            else
-            {
-                Console.WriteLine("Bebida não encontrada");
-            }
-        }
-
-        public static void ExcluirBebida(int id)
-        {
-            var bebida = ListaBebida.FirstOrDefault(b => b.Id == id);
-            if (bebida != null)
-            {
-                ListaBebida.Remove(bebida);
-            }
-            else
-            {
-                Console.WriteLine("Bebida não encontrada");
-            }
-        }
-
-        public static List<Bebida> ListarBebidas()
-        {
-            return ListaBebida;
-        }
-
-        public static void ImprimirRefrigerantes()
-        {
-            foreach (var refrigerante in ListaRefrigerante)
-            {
-                refrigerante.ImprimirDados();
-            }
-        }
-    }
 }
-
+}
